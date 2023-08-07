ce8afaad957172a27806aadc94b79e6fc01a000b
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-07 07:53:11 +0000

    [Add] Topページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,14 @@ class BooksController < ApplicationController
 # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
