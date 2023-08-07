b2fbff7f668fcf99ba2b76831a629eb6854269c9
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-07 00:52:02 +0000

    [Finish] Bookers投稿機能のコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'book/new'
-  get 'book/index'
-  get 'book/show'
-  get 'book/edit'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   get "/" => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
